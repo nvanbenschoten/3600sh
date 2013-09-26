ffe5f3332c8db251efe2c3be1f304361c2532ea4
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2013-09-26 13:06:40 -0400

    Commented and added better return values

--- 3600sh.c
+++ 3600sh.c
@@ -29,54 +29,42 @@ int main(int argc, char*argv[]) {
 			return 1;
 		} else if (ret == 2) {
 			break;
-		} else {
-			char **args;
-
-			int backgroundProc = 0;
-
-			ret = do_parse_input(input, &args, &backgroundProc);
-			if (ret == 1) {
-				printf("%s\n", "ERROR");
-				
-			} else if (ret == 2) {
-				printf("%s\n", "Error: Unrecognized escape sequence.");
-			} else if (ret == 3) {
-				printf("%s\n", "Error: Invalid syntax.");
-			}
-			if (ret) {
-				if (eof) {
-					break;
-				} else {
-					continue;
-				}
-			}
-			free(input);
-
-			// If no args, continue loop
-			if (args[0] == NULL) {
-				free_args(args);
-				continue;
-			}
-
-			if (!strcmp(args[0], "exit")) {
-				// Need to also support EOF
-				// Handle \n EOF
-				free_args(args);
-				break;	
-			} else {
-				//char * path = calloc(50, sizeof(char));
-				//strcpy(path, "");
-
-				do_exec(args, backgroundProc);
-				
-				//free(path);
-				free_args(args);
-				if (eof) {
-					break;
-				}
-			}
 		}
 
+		// Parsing input
+		char **args;
+		int backgroundProc = 0;
+		ret = do_parse_input(input, &args, &backgroundProc);
+		free(input);
+		if (ret == 1) {
+			return 1;	
+		} else if (ret == 2) {
+			if (eof) {
+				break;
+			} else {
+				continue;
+			}
+		}
+		
+		// If no args, continue loop
+		if (args[0] == NULL) {
+			free_args(args);
+			continue;
+		}
+
+		if (!strcmp(args[0], "exit")) {
+			free_args(args);
+			break;	
+		} else {
+			// Cals do_exec
+			do_exec(args, backgroundProc);
+			
+			free_args(args);
+			if (eof) {
+				// If end of file, end
+				break;
+			}
+		}
 		
   }
 
@@ -87,29 +75,47 @@ int main(int argc, char*argv[]) {
 }
 
 // Function which prompts the user for input
-//
+// Return codes:
+//   1 - Program failure, exit immediately
+//   2 - Program exit after printing exit
 int do_prompt(char **input, int *eof) {
 	// Return code variable
 	int ret;
 
 	// Getting the username
-	printf("%s@", getenv("USER"));
+	char *user = getenv("USER");
+	if (user == NULL) {
+		printf("%s\n", "Error: Username acquisition failed.");
+		return 1;
+	}
+	printf("%s@", user);
 
 	// Getting the hostname
 	// Max length according to posix = 255 bytes
 	char *host = (char *)calloc(256, sizeof(char));
-	ret = gethostname(host, 256);
-	host[255] = '\0';
-	// Checking for errors
-	if (ret) {
+	if (host == NULL) {
+		// Checking memory allocation
+		printf("%s\n", "Error: Memory allocation failed.");
 		return 1;
 	}
+	ret = gethostname(host, 256);
+	if (ret) {
+		printf("%s\n", "Error: Hostname acquisition failed.");
+		return 1;
+	}
+	host[255] = '\0';
 
 	// Getting the current working directory
 	// Max length of path is 4096
 	char *dir = (char *)calloc(4096, sizeof(char));
+	if (dir == NULL) {
+		// Checking memory allocation
+		printf("%s\n", "Error: Memory allocation failed.");
+		return 1;
+	}
 	// Checking for errors
 	if (getcwd(dir, 4096) != dir) {	
+		printf("%s\n", "Error: Current working directory acquisition failed.");
 		return 1;
 	}
 	dir[4095] = '\0'; 
@@ -126,49 +132,66 @@ int do_prompt(char **input, int *eof) {
 	char * temp = "";
 	do {
 		c = getc(stdin);
-		if (strlen(*input) == 0) { // if this is the first char of input
+		if (strlen(*input) == 0) { 
+			// if this is the first char of input
 			if (c == EOF) {
-				// If first
+				// If first and end of file
 				if (feof(stdin)) {
 					*eof = 1;
-					//printf("\n");
+					// Return and exit properly
 					return 2;
 				} else {
+					printf("%s\n", "Error: Read failed from getc.");
 					return 1;
 				}
 			}
-			*input = (char *) calloc(2, sizeof(char)); // allocate space for input string
+			// allocate space for input string
+			*input = (char *) calloc(2, sizeof(char)); 
+			if (input == NULL) {
+				// Checking memory allocation
+				printf("%s\n", "Error: Memory allocation failed.");
+				return 1;
+			}
 			(*input)[0] = (char) c; // write char and null terminator to input string
 			(*input)[1] = '\0';
-		} else { // otherwise we have previously allocated memory
+		} else { 
+			// otherwise we have previously allocated memory
 			if (c == EOF) {
-				// If not first
+				// If EOF but not first
 				if (feof(stdin)) {
 					*eof = 1;
 					temp = *input;
-					*input = (char *) calloc(strlen(temp) + 2, sizeof(char)); // add more space to input
+					// add more space to input so that it can be terminated
+					*input = (char *) calloc(strlen(temp) + 2, sizeof(char)); 
+					if (input == NULL) {
+						// Checking memory allocation
+						printf("%s\n", "Error: Memory allocation failed.");
+						return 1;
+					}
 					strcpy(*input, temp); // copy back over input string
 					(*input)[strlen(temp)] = '\n'; // add newly read char
 					(*input)[strlen(temp) + 1] = '\0'; // add null terminator
 					free(temp);
-					//printf("\n");
 					return 0;
 				} else {
+					printf("%s\n", "Error: Read failed from getc.");
 					return 1;
 				}
 			}
 			temp = *input;
 			*input = (char *) calloc(strlen(temp) + 2, sizeof(char)); // add more space to input
+			if (input == NULL) {
+				// Checking memory allocation
+				printf("%s\n", "Error: Memory allocation failed.");
+				return 1;
+			}
 			strcpy(*input, temp); // copy back over input string
 			(*input)[strlen(temp)] = c; // add newly read char
 			(*input)[strlen(temp) + 1] = '\0'; // add null terminator
 			free(temp);
 		}
 	} while (c != '\n' && c != EOF);
-	// input will include \n char on end
-
-	// DEBUG
-	//printf("%s", *input);
+	// Input will include \n char on end
 	   
 	return 0;
 }
@@ -176,23 +199,30 @@ int do_prompt(char **input, int *eof) {
 // Function which parses the user input from a string to usable data
 // Return Codes:
 //	0 - Good
-// 	1 - Generic Error
-//	2 - Escape Sequence Error 
-// 	3 - & Syntax Error
+// 	1 - Exit error
+//	2 - Reprompt error
 int do_parse_input(char *input, char ***args, int *background) {
 	// Compiling regular expression
 	regex_t r;
-	char * regex_text = "[ \t]*[^ \t\n]+[ \t\n]+"; //[-A-Za-z0-7_&/\\]
+	// Regex set to one or more non space characters then at least on space character
+	char * regex_text = "[ \t]*[^ \t\n]+[ \t\n]+";
 	int ret = regcomp (&r, regex_text, REG_EXTENDED|REG_NEWLINE);
-	if (ret) // Returns if error with regex compilation
+	if (ret) {
+		// Returns if error with regex compilation
+		printf("%s\n", "Error: Regex compilation failed.");
 		return 1;
+	}
 
-	// Creating args string pointer array
-	*args = (char **) calloc(1, sizeof(char *));
-	if (*args == NULL)
-		return 1;
 	// Creates arg count
 	int argc = 0;
+	// Creating args array string pointer array
+	*args = (char **) calloc(argc + 1, sizeof(char *));
+	if (args == NULL) {
+		// Checking memory allocation
+		printf("%s\n", "Error: Memory allocation failed.");
+		return 1;
+	}
+
 	// Last argument is null pointer
 	(*args)[0] = (char *) NULL;
 	// Setting background to no
@@ -201,6 +231,8 @@ int do_parse_input(char *input, char ***args, int *background) {
 	// Cursor pointer
 	char * pointer = input;
 	int i = 0;
+
+	// Combine with prev will be used if 
 	int combineWithPrev = 0;
 
 	while (1) {
@@ -220,82 +252,127 @@ int do_parse_input(char *input, char ***args, int *background) {
 		combineWithPrev = 0;
 
 		for (i = 0; match[0].rm_so + i < match[0].rm_eo; i++) {
-			if (*background && (*(pointer + match[0].rm_so + i)) != '\n' && (*(pointer + match[0].rm_so + i)) != ' ' && (*(pointer + match[0].rm_so + i)) != '\t')
-				return 3;
+			// Iterates through match token
+			if (*background && (*(pointer + match[0].rm_so + i)) != '\n' && (*(pointer + match[0].rm_so + i)) != ' ' && (*(pointer + match[0].rm_so + i)) != '\t') {
+				// Character after &
+				printf("%s\n", "Error: Invalid syntax.");
+				return 2;
+			}	
 
-			int length = 0;
-			if (strlen(matchString) == 0) { // if this is the first char of match
-				matchString = (char *) calloc(2, sizeof(char)); // allocate space for input string
+			int prevlength = 0;
+
+			if (strlen(matchString) == 0) { 
+				// If this is the first char of match
+				matchString = (char *) calloc(2, sizeof(char)); 
+				// Allocate space for input string
+				if (matchString == NULL) {
+					// Checking memory allocation
+					printf("%s\n", "Error: Memory allocation failed.");
+					return 1;
+				}
 			} else { 
-				// otherwise we have previously allocated memory
+				// Otherwise we have previously allocated memory
 				temp = matchString;
-				matchString = (char *) calloc(strlen(temp) + 2, sizeof(char)); // add more space to input
-				strcpy(matchString, temp); // copy back over input string
-				length = strlen(temp);
+				matchString = (char *) calloc(strlen(temp) + 2, sizeof(char)); 
+				// Add more space to input
+				if (matchString == NULL) {
+					// Checking memory allocation
+					printf("%s\n", "Error: Memory allocation failed.");
+					return 1;
+				}
+				// Copy back over input string
+				strcpy(matchString, temp); 
+				// Set prevlength variable
+				prevlength = strlen(temp);
 				free(temp);
 			}
 
 			switch (*(pointer + match[0].rm_so + i)) {
 				case '\n':
-					matchString[length] = '\0';
+					// Ends string
+					matchString[prevlength] = '\0';
 					break;
 				case '\\':
 					// Deal with escape characters
+					// Needs to increment index to account for dealing with next char
 					i++;
 					if (!(match[0].rm_so + i < match[0].rm_eo)) {
-						// End of line, get the hell out of there
+						// End of line, get out of there with error
+						printf("%s\n", "Error: Unrecognized escape sequence.");
 						return 2;
 					} else {
 						switch (*(pointer + match[0].rm_so + i)) {
 							// Next char is available
 							case '\\':
-								matchString[length] = '\\';
+								// Sets char to backslash
+								matchString[prevlength] = '\\';
 								break;
 							case ' ':
-								matchString[length] = ' ';
+								// Sets char to space
+								matchString[prevlength] = ' ';
 								combineWithPrev = 1;
 								break;
 							case 't':
-								matchString[length] = '\t';
+								// Sets char to \t
+								matchString[prevlength] = '\t';
 								break;
 							case '&':
-								matchString[length] = '&';
+								// Sets char to &
+								matchString[prevlength] = '&';
 								break;
 							default:
+								// Anything else, error
+								printf("%s\n", "Error: Unrecognized escape sequence.");
 								return 2;
 						}
 					} 
 					break;
 				case '&':
-					matchString[length] = '&';
+					// Adds to end of string and sets background flag
+					matchString[prevlength] = '&';
 					*background = 1;
 					break;
 				case ' ':
+					// Do nothing with spaces, should not occur due to regex except at end of token
 					break;
 				case '\t':
+					// Do nothing with \t, should not occur due to regex except at end of token
 					break;
 				default:
-					matchString[length] = *(pointer + match[0].rm_so + i); // add newly read char
+					// Sets string char to char in token
+					matchString[prevlength] = *(pointer + match[0].rm_so + i); // add newly read char
 					break;
 			}
 				
-			matchString[length + 1] = '\0'; // add null terminator
-			
+			// add null terminator to next character	
+			matchString[prevlength + 1] = '\0';
 		}
-
-
 
 		// Adds string to string pointer array
 		if (combine && *(pointer-2) == '\\') {
+			// If token needs to be combines with previous
 			char *tempPointer = (*args)[argc-2];
-			(*args)[argc-2] = (char *)calloc(strlen((*args)[argc-2]) + sizeof(matchString) + 1, sizeof(char));
+			// Reallocates previous token with length of each token and room for null terminator
+			(*args)[argc-2] = (char *)calloc(strlen(tempPointer) + strlen(matchString) + 1, sizeof(char));
+			if ((*args)[argc-2] == NULL) {
+				// Checking memory allocation
+				printf("%s\n", "Error: Memory allocation failed.");
+				return 1;
+			}
 			strcpy((*args)[argc-2], tempPointer);
 			free(tempPointer);
 			strcat((*args)[argc-2], matchString);
 			argc--;
 		} else {
 			char **tempArgs = *args;
-			*args = (char **) calloc(argc + 1, sizeof(char*)); // move args back to array
+			// Needs to reallocate arg array with size for argc plus room for null pointer
+			*args = (char **) calloc(argc + 1, sizeof(char*));
+			if (*args == NULL) {
+				// Checking memory allocation
+				printf("%s\n", "Error: Memory allocation failed.");
+				return 1;
+			}
+			// Copies args back from temp arg array
 			for (i = 0; i < argc - 1; i++)
 				(*args)[i] = tempArgs[i];
 			(*args)[i] = matchString;
@@ -308,21 +385,27 @@ int do_parse_input(char *input, char ***args, int *background) {
 
 	}
 
+	// Frees regular expression
 	regfree(&r);
 
 	// Fix & at end
 	if (*background) {
 		if (strlen((*args)[argc-1]) == 1) {
-			// Delete term
+			// Delete term if alone
 			argc--;
 			char **tempAmp = *args;
 			*args = (char **) calloc(argc + 1, sizeof(char*)); // move args back to array
+			if (*args == NULL) {
+				// Checking memory allocation
+				printf("%s\n", "Error: Memory allocation failed.");
+				return 1;
+			}
 			int j = 0;
 			for (j = 0; j < argc; j++)
 				(*args)[j] = tempAmp[j];
 			(*args)[j] = (char *) NULL;
 		} else {
-			// Delete last byte
+			// Delete last byte if in token
 			(*args)[argc-1][strlen((*args)[argc-1])-1] = '\0';
 		}
 	}
@@ -464,18 +547,22 @@ void do_exit() {
 //
 void free_args(char **args) {
 	int i;
+	// Frees args up to null
 	for (i = 0; args[i] != NULL; i++) {
 		free(args[i]);
 	}  
+	// Frees null pointer
 	free(args[i]);  
+	// Frees args array pointer
 	free(args);
 }
 
 void debug_print_args(char **args) {
+	// Prints args with quotes around them
 	int i = 0;
 	printf("%s\n", "Printing Args");
 	for (i = 0; args[i] != NULL; i++) {
-		printf("\t%sEND\n", args[i]);
+		printf("\t\"%s\"\n", args[i]);
 	}
 	printf("\t%s\n", args[i]);
 }
